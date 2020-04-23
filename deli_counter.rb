926d615aac6095e2f03d5d893801a782a052d200
Christine Zosche <christine.zosche@gmail.com>
2020-04-23 01:45:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,9 @@ end
 def take_a_number(katz_deli, name)
   other_deli = [katz_deli]
   if katz_deli.length == 0
-    other_deli << name
+    other_deli << "#{name}"
   elsif katz_deli.length >= 1
-    other_deli.push name
+    other_deli.push("#{name}")
   end
   other_deli.each_with_index do |customer, index|
     puts "Welcome, #{customer}. You are number #{index+1} in line."
