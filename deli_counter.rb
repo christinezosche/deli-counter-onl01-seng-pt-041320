370aade9b94fe524e5996bc0e10b79004be305d8
Christine Zosche <christine.zosche@gmail.com>
2020-04-23 01:13:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
+    new_line = []
     katz_deli.each_with_index do |name, idx|
      puts "The line is currently: #{idx+1}. #{name}"
     end
