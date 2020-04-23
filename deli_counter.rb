755ec1132eec99f4f34816a345c2ad73765f686f
Christine Zosche <christine.zosche@gmail.com>
2020-04-23 01:46:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  other_deli.each_with_index do |customer, index|
+  katz_deli.each_with_index do |customer, index|
     puts "Welcome, #{customer}. You are number #{index+1} in line."
   end
 end
