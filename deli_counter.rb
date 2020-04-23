c0e0af1bf0006b75e6f6b891abdaf146cc460f78
Christine Zosche <christine.zosche@gmail.com>
2020-04-23 01:58:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,4 +21,5 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length >= 1
-    puts "Currently serving #{"
+    puts "Currently serving #{array[0]}."
+    katz_deli
